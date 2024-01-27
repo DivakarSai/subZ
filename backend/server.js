0e9bd8e0c03b2a36ffdfeb7e42599cf157470019
DivakarSai <divakarsainukala@gmail.com>
2024-01-27 22:31:00 +0530

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
-const express = require('express');
+import express from 'express';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 5001;
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
